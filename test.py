80202ea8aad48af21bb6e67ed771745ca5a66226
gabolabo <ggaldames058@github.com>
2026-04-22 11:44:44 -0400

    se quitan cosas inesesarias

--- test.py
+++
@@ -1,7 +0,0 @@
-matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]
-          ]
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        print("Fila:", i,"columna:", j, "valor:", matrix[i][j])
